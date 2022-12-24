643bbca9ed8cadac03a46a09ee372a2b64b9aba1
MiracleHorizon <alexeenkodenisjke@gmail.com>
2022-12-24 21:06:32 +0300

    Патч от 24.12.2022. Server.
    Prisma.
    
    update: prisma обновлена до последней версии (4.8.0).
    
    feature: в модель User добавлены следующие поля: login, createdAt, updatedAt, reactCoinsCount, phoneNumber.
    
    refactor: модель Address переименована в DeliveryAddress. Соответствующие связи также были переименованы.
    
    feature: в модель DeliveryAddress добавлены следующие поля: intercom, createdAt, updatedAt.
    
    feature: в модель BillingCard добавлены следующие поля: createdAt, updatedAt.
    
    fix: поле expireDate в модели BillingCard исправлено на expiredDate.
    
    feature: в модель Order добавлены следующие поля: recipientPhoneNumber, recipientName, commentary, updatedAt.
    
    feature: в модель OrderProduct добавлены следующие поля: createdAt, updatedAt.

---
+++ server/prisma/migrations/20221224175355_init/migration.sql
@@ -0,0 +1,134 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "BillingCardType" AS ENUM ('DEFAULT', 'GOLD');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('ASSEMBLY', 'ON_THE_WAY', 'DELIVERED', 'CANCELED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "reactCoinsCount" INTEGER NOT NULL DEFAULT 0,
+    "phoneNumber" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "deliveryAddresses" (
+    "id" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "house" TEXT NOT NULL,
+    "entrance" INTEGER NOT NULL,
+    "floor" INTEGER NOT NULL,
+    "flatOrOffice" INTEGER NOT NULL,
+    "intercom" INTEGER,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "deliveryAddresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "billingCards" (
+    "id" TEXT NOT NULL,
+    "number" INTEGER[],
+    "cvc" INTEGER NOT NULL,
+    "expiredDate" TIMESTAMP(3) NOT NULL,
+    "type" "BillingCardType" NOT NULL DEFAULT 'DEFAULT',
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "billingCards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" TEXT NOT NULL,
+    "deliveryAddress" TEXT NOT NULL,
+    "cutleryCount" INTEGER NOT NULL DEFAULT 0,
+    "totalPrice" DOUBLE PRECISION NOT NULL,
+    "productsPrice" DOUBLE PRECISION NOT NULL,
+    "deliveryPrice" DOUBLE PRECISION NOT NULL,
+    "serviceFee" DOUBLE PRECISION NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'ASSEMBLY',
+    "recipientPhoneNumber" TEXT NOT NULL,
+    "recipientName" TEXT NOT NULL,
+    "commentary" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orderProducts" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "count" INTEGER NOT NULL,
+    "imageUrl" TEXT,
+    "orderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orderProducts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "productCategories" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "productCategories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "price" INTEGER NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "tag" TEXT NOT NULL,
+    "productCategoryId" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phoneNumber_key" ON "users"("phoneNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "productCategories_title_key" ON "productCategories"("title");
+
+-- AddForeignKey
+ALTER TABLE "deliveryAddresses" ADD CONSTRAINT "deliveryAddresses_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "billingCards" ADD CONSTRAINT "billingCards_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orderProducts" ADD CONSTRAINT "orderProducts_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_productCategoryId_fkey" FOREIGN KEY ("productCategoryId") REFERENCES "productCategories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
