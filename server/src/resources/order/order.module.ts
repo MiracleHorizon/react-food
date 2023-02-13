4c05157d26e1855ad06dbbe88470f0423057218e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-02-13 14:45:07 +0300

    Server. 13.02.2023.
    Улучшения и исправления для деплоя.

--- server/src/resources/order/order.module.ts
+++ server/src/resources/order/order.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common'
 
 import { OrderController } from './order.controller'
 import { OrderService } from './order.service'
+import { PrismaService } from 'prisma/prisma.service'
+import { CartService } from '@/resources/cart/cart.service'
+import { ProductService } from '@/resources/product/product.service'
+import { UsersService } from '@/resources/users/users.service'
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [
+    OrderService,
+    PrismaService,
+    CartService,
+    ProductService,
+    UsersService
+  ]
 })
 export class OrderModule {}
