7195dc2e4fb1744abb210edabd04e1c76c13ae91
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-05-29 18:03:19 +0300

    Client
    refactor: удалена папка lib

--- client/lib/environment/index.ts
+++
@@ -1,3 +0,0 @@
-export { fetchEnvironmentData } from './fetch-environment-data'
-export { initializeEnvironmentData } from './initialize-environment-data'
-export type { EnvironmentData } from './fetch-environment-data'
