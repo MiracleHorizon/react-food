04234a54a6930d0353f5ed69a4a9e057c4403298
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-05-29 18:02:47 +0300

    Client
    refactor: проведен рефакторинг eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,11 +1,26 @@
-module.exports = {
+const eslintConfig = {
   extends: [
     'next/core-web-vitals',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended'
   ],
+  plugins: ['react', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'jsx-quotes': ['error', 'prefer-single']
   }
 }
+
+module.exports = eslintConfig
