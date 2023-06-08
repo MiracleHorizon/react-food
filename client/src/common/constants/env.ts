414dbad7a85723a71fc9b516d8f44784f62f93fb
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-08 16:29:46 +0300

    Client. 08.06.2023.
    refactor: убраны моковые значения для констант CLIENT_API, SERVER_API

--- client/src/common/constants/env.ts
+++ client/src/common/constants/env.ts
@@ -1,5 +1,2 @@
-const MOCK_CLIENT_API: string = 'http://localhost:3000'
-const MOCK_SERVER_API: string = 'http://localhost:4200'
-
-export const CLIENT_API: string = process.env.CLIENT_API ?? MOCK_CLIENT_API
-export const SERVER_API: string = process.env.SERVER_API ?? MOCK_SERVER_API
+export const CLIENT_API: string = process.env.CLIENT_API as string
+export const SERVER_API: string = process.env.SERVER_API as string
