6660807ae5416d33a99d6d8fb424662ce898848a
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-09 15:04:20 +0300

    Client. 09.06.2023.
    lib: установлена библиотека usehooks-ts
    
    feat: добавлены новые компоненты svg-иконок

---
+++ client/src/components/ui/svg/XMarkSvg.tsx
@@ -0,0 +1,19 @@
+const XMarkSvg = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    width={32}
+    height={32}
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M6 18L18 6M6 6l12 12'
+    />
+  </svg>
+)
+
+export default XMarkSvg
