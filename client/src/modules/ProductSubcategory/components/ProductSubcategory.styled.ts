2b6ee30222fc27860d04f98112c31598c40557f9
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-13 23:48:54 +0300

    Client. 13.06.2023.
    feat: для страницы подкатегорий продуктов добавлена адаптивная верстка header`a

--- client/src/modules/ProductSubcategory/components/ProductSubcategory.styled.ts
+++ client/src/modules/ProductSubcategory/components/ProductSubcategory.styled.ts
@@ -4,4 +4,9 @@ export const Root = styled.main`
   max-width: ${300 * 4}px;
   min-width: calc(100vw - 240px - 110px - 320px - 50px);
   padding-bottom: 10px;
+
+  @media screen and (max-width: 1240px) {
+    max-width: initial;
+    width: 100%;
+  }
 `
--- client/src/modules/ProductSubcategory/components/ProductSubcategoryHeader/ProductSubcategoryHeader.styled.ts
+++ client/src/modules/ProductSubcategory/components/ProductSubcategoryHeader/ProductSubcategoryHeader.styled.ts
@@ -1,11 +1,35 @@
 import styled from '@emotion/styled'
 
+import { maxDeviceWidth } from '@styles/responsiveness/devices'
+
 export const Root = styled.header`
   margin-bottom: 18px;
+
+  @media screen and (${maxDeviceWidth.tablet}) {
+    margin-bottom: 11px;
+  }
+
+  @media screen and (max-width: 517px) {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
 `
 
 export const Title = styled.h1`
   margin-top: 8px;
   font-size: 44px;
   font-weight: 600;
+
+  @media screen and (max-width: 1025px) {
+    font-size: 38px;
+  }
+
+  @media screen and (${maxDeviceWidth.tablet}) {
+    font-size: 34px;
+  }
+
+  @media screen and (max-width: 517px) {
+    font-size: 30px;
+  }
 `
