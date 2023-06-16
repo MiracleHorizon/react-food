acb6a54f0145fcb3bf22ccf700b99296fb28cada
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-16 14:01:06 +0300

    Client. 16.06.2023.
    refactor: проведен рефакторинг стилей компонента ProductsGrid

--- client/src/components/products/ProductsGrid/ProductsGrid.styled.ts
+++ client/src/components/products/ProductsGrid/ProductsGrid.styled.ts
@@ -1,19 +1,26 @@
 import styled from '@emotion/styled'
 
 import ShowcaseProductCard from '@components/products/ShowcaseProductCard'
+import { maxDeviceWidth } from '@styles/responsiveness/devices'
 
 const minCardWidth = 224
 
 export const Root = styled.div`
   width: 100%;
   display: grid;
-  justify-items: center;
   grid-template-columns: repeat(auto-fill, minmax(${minCardWidth}px, 1fr));
   gap: 10px;
+
+  @media screen and (${maxDeviceWidth.tablet}) {
+    justify-items: center;
+  }
 `
 
 export const Card = styled(ShowcaseProductCard)`
   min-width: ${minCardWidth}px;
-  max-width: 300px;
   width: 100%;
+
+  @media screen and (max-width: 550px) {
+    max-width: 300px;
+  }
 `
