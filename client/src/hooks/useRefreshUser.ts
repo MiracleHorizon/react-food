295b0ff8b6fcf46e2c74dec951395eec7c3d3de3
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-29 17:14:51 +0300

    Client. 29.06.2023.
    feat: добавлены новые опции и условия запроса refresh пользователя
    
    fix: исправлено значение ревалидации запроса refresh пользователя.

--- client/src/hooks/useRefreshUser.ts
+++ client/src/hooks/useRefreshUser.ts
@@ -1,19 +1,28 @@
 import useSWR from 'swr'
+import Cookie from 'js-cookie'
 
 import { authService } from '@api/AuthService'
 import { useUserStore } from '@stores/userStore'
 import { useCartStore } from '@stores/cartStore'
-import { ACCESS_TOKEN_EXPIRES_TIME } from '@constants/cookie'
+import {
+  ACCESS_TOKEN_EXPIRES_TIME,
+  REFRESH_TOKEN_COOKIE_NAME
+} from '@constants/cookie'
 
 export const useRefreshUser = () => {
+  const refreshToken = Cookie.get(REFRESH_TOKEN_COOKIE_NAME)
+
   const signin = useUserStore(state => state.signin)
   const isAuth = useUserStore(state => state.isAuth())
   const initializeCart = useCartStore(state => state.initialize)
 
   const fetcher = () => authService.refresh()
 
-  useSWR(!isAuth ? 'refreshUser' : null, fetcher, {
+  useSWR(!isAuth && refreshToken ? 'refreshUser' : null, fetcher, {
     refreshInterval: ACCESS_TOKEN_EXPIRES_TIME,
+    revalidateOnReconnect: true,
+    revalidateOnFocus: false,
+    refreshWhenHidden: false,
     onSuccess({ user, cart }) {
       signin(user)
       initializeCart(cart)
